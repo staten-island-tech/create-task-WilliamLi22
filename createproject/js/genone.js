1a4239c5b5df943efcdaddadfefad1a1cff97db5
WilliamLi22 <forvalious@gmail.com>
2023-02-28 10:12:50 -0500

    Reject api, return to list

---
+++ createproject/js/genone.js
@@ -0,0 +1,193 @@
+const pokemon = [
+  {
+    id: 0,
+    name: "Bulbasaur",
+    types: "Grass" + "Poison",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    id: 1,
+    name: "Ivysaur",
+    types: "Grass" + "Poison",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+  },
+  {
+    id: 2,
+    name: "Venusaur",
+    types: "Grass" + "Poison",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+  },
+  {
+    id: 3,
+    name: "Charmander",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    id: 4,
+    name: "Charmeleon",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+  },
+  {
+    id: 5,
+    name: "Venusaur",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+  },
+  {
+    id: 6,
+    name: "Squirtle",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+  },
+  {
+    id: 7,
+    name: "Wartortle",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+  },
+  {
+    id: 8,
+    name: "Blastoise",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+  },
+  {
+    id: 9,
+    name: "Chikorita",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png",
+  },
+  {
+    id: 10,
+    name: "Bayleef",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/153.png",
+  },
+  {
+    id: 11,
+    name: "Meganium",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/154.png",
+  },
+  {
+    id: 12,
+    name: "Cyndaquil",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/155.png",
+  },
+  {
+    id: 13,
+    name: "Quilava",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/156.png",
+  },
+  {
+    id: 14,
+    name: "Typhlosion",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/157.png",
+  },
+  {
+    id: 15,
+    name: "Totodile",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/158.png",
+  },
+  {
+    id: 16,
+    name: "Croconaw",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/159.png",
+  },
+  {
+    id: 17,
+    name: "Feraligatr",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/160.png",
+  },
+  {
+    id: 9,
+    name: "Chikorita",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png",
+  },
+  {
+    id: 10,
+    name: "Bayleef",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/153.png",
+  },
+  {
+    id: 11,
+    name: "Meganium",
+    types: "Grass" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/154.png",
+  },
+  {
+    id: 12,
+    name: "Cyndaquil",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/155.png",
+  },
+  {
+    id: 13,
+    name: "Quilava",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/156.png",
+  },
+  {
+    id: 14,
+    name: "Typhlosion",
+    types: "Fire" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/157.png",
+  },
+  {
+    id: 15,
+    name: "Totodile",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/158.png",
+  },
+  {
+    id: 16,
+    name: "Croconaw",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/159.png",
+  },
+  {
+    id: 17,
+    name: "Feraligatr",
+    types: "Water" + "N/A",
+    front_sprite:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/160.png",
+  },
+];
+
+export { menu };
